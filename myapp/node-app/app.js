f9758a4666a5cf99d8bca357ab0f2e20dc7828ed
Harsh Yadav <harshcode95@gmail.com>
2026-04-18 07:27:52 +0000

    Fixed port for Render deployment

--- myapp/node-app/app.js
+++ myapp/node-app/app.js
@@ -39,4 +39,7 @@ const server = http.createServer(async (req, res) => {
   res.end();
 });
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
